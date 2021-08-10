b1911e834bd626a53035c86b71a962b10415978e
dlshnkLK <dilshankaliyanage@yahoo.com>
2021-08-10 19:07:42 +0530

    mobile done

--- main.js
+++ main.js
@@ -1,20 +1,82 @@
 // var numberNotChecked = $('input:checkbox:not(":checked")').length;
 // console.log(numberNotChecked);
 
-$('input:checkbox:not(":checked")').change(function () {
-    var numberNotChecked = $('input:checkbox:not(":checked")').length;
-    console.log(20 - numberNotChecked);
-
-    let final = (20 - numberNotChecked)
-
-    $('#count-checked-checkboxes').text(final);
-
-    if (final > 5) {
-        $(".count-check").addClass('text-red-500');
-    } else {
-        $(".count-check").removeClass('text-red-500');
-    }
 
 
+const desktop = () => {
+    $('input:checkbox:not(":checked")').change(function () {
+        var numberNotChecked = $('input:checkbox:not(":checked")').length;
+        console.log(20 - numberNotChecked);
+
+        let final = (20 - numberNotChecked)
+
+        $('#count-checked-checkboxes').text(final);
+
+        if (final > 5) {
+            $(".count-check").addClass('text-red-500');
+        } else {
+            $(".count-check").removeClass('text-red-500');
+        }
+    });
+}
+
+desktop();
+
+
+const mobile = () => {
+    $('input:checkbox:not(":checked")').change(function () {
+        var numberNotChecked = $('input:checkbox:not(":checked")').length;
+        console.log(20 - numberNotChecked);
+
+        let final = (20 - numberNotChecked)
+
+        $('#count-checked-checkboxes-mobile').text(final);
+
+        if (final > 5) {
+            $(".count-check-mobile").addClass('text-red-500');
+        } else {
+            $(".count-check-mobile").removeClass('text-red-500');
+        }
+    });
+}
+
+mobile();
+
+
+var height = $(window).height();
+var width = $(window).width(); //768
+
+if (width > 768) {
+    let remDom = document.getElementById("checklist-mobile");
+    remDom.remove();
+} else {
+    let remDom = document.getElementById("checklist");
+    remDom.remove();
+}
+
+//refresh screen web resizing
+$(window).resize(function () {
+    location.reload(true)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
